288e8516118c9d11a29e4ecba6d9f37de2aebc9a
Aditya Deshmukh <adidsoff.ind@gmail.com>
2026-03-06 16:26:46 +0530

    Preparing for deployment

--- server/server.js
+++ server/server.js
@@ -2,31 +2,28 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
+import path from 'path';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
 import { Server } from 'socket.io';
 
-//create express app using express server
 const app = express();
 const server = http.createServer(app);
+const __dirname = path.resolve();
 
-//initialise socket.io server
 export const io = new Server(server, {
     cors: {origin: "*"}
 });
 
-//store online users
-export const userSocketMap = {}; //{userId: socketId}
+export const userSocketMap = {};
 
-//Socket.io connection handler
 io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User connected", userId);
 
     if(userId) userSocketMap[userId] = socket.id;
 
-    //emit online users to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
@@ -36,25 +33,25 @@ io.on("connection", (socket) => {
     })
 });
 
-//middleware setup
-app.use(express.json({
-    limit: "4mb"
-}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
 
-//Route setup
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-//connect to mongodb
-await connectDB();
-
-if(process.env.NODE_ENV !== 'production'){
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => console.log("Server is running on port" + PORT));
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+    })
 }
 
-//export server for vercel deployment
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server running on port " + PORT));
+
 export default server;
